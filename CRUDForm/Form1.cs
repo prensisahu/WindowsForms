cbb0064a8c4bb372f2e3bd67117d495ab16a09f8
prensi <prensi2tap@gmail.com>
2025-03-13 04:17:08 +0530

    Update Form1.cs

--- CRUDForm/Form1.cs
+++ CRUDForm/Form1.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Data;
 using System.Data.SqlClient;
+using System.Windows.Forms;
 
 namespace CRUDForm
 {
@@ -10,54 +12,86 @@ namespace CRUDForm
             InitializeComponent();
         }
 
+        private bool ValidateInputs()
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text) ||
+                string.IsNullOrWhiteSpace(textBox2.Text) ||
+                string.IsNullOrWhiteSpace(textBox3.Text))
+            {
+                MessageBox.Show("Please enter all fields (ID, Name, Age)", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            return true;
+        }
+
         private void Insert_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection("Data Source=DESKTOP-IFQAM6G;Initial Catalog=CRUDForm;Integrated Security=True;Encrypt=False");
-            con.Open();
-            SqlCommand cmd = new SqlCommand("Insert into ut values(@id,@name,@age)", con);
-            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
-            cmd.Parameters.AddWithValue("@name", textBox2.Text);
-            cmd.Parameters.AddWithValue("@age", int.Parse(textBox3.Text));
-            cmd.ExecuteNonQuery();
-            con.Close();
-            MessageBox.Show("Succesfully saved");
+            if (!ValidateInputs()) return;
+
+            using (SqlConnection con = new SqlConnection("Data Source=DESKTOP-IFQAM6G;Initial Catalog=CRUDForm;Integrated Security=True;Encrypt=False"))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Insert into ut values(@id,@name,@age)", con);
+                cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+                cmd.Parameters.AddWithValue("@name", textBox2.Text);
+                cmd.Parameters.AddWithValue("@age", int.Parse(textBox3.Text));
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Successfully saved");
+            }
         }
 
         private void Update_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection("Data Source=DESKTOP-IFQAM6G;Initial Catalog=CRUDForm;Integrated Security=True;Encrypt=False");
-            con.Open();
-            SqlCommand cmd = new SqlCommand("Update ut set name=@name, age=@age Where id=@id", con);
-            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
-            cmd.Parameters.AddWithValue("@name", textBox2.Text);
-            cmd.Parameters.AddWithValue("@age", int.Parse(textBox3.Text));
-            cmd.ExecuteNonQuery();
-            con.Close();
-            MessageBox.Show("Update succesfully");
+            if (!ValidateInputs()) return;
+
+            using (SqlConnection con = new SqlConnection("Data Source=DESKTOP-IFQAM6G;Initial Catalog=CRUDForm;Integrated Security=True;Encrypt=False"))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Update ut set name=@name, age=@age Where id=@id", con);
+                cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+                cmd.Parameters.AddWithValue("@name", textBox2.Text);
+                cmd.Parameters.AddWithValue("@age", int.Parse(textBox3.Text));
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Updated successfully");
+            }
         }
 
         private void Delete_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection("Data Source=DESKTOP-IFQAM6G;Initial Catalog=CRUDForm;Integrated Security=True;Encrypt=False");
-            con.Open();
-            SqlCommand cmd = new SqlCommand("Delete ut Where id=@id", con);
-            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
-            cmd.ExecuteNonQuery();
-            con.Close();
-            MessageBox.Show("Delete succesfully");
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Please enter ID to delete", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            using (SqlConnection con = new SqlConnection("Data Source=DESKTOP-IFQAM6G;Initial Catalog=CRUDForm;Integrated Security=True;Encrypt=False"))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Delete from ut Where id=@id", con);
+                cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Deleted successfully");
+            }
         }
 
         private void Search_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection("Data Source=DESKTOP-IFQAM6G;Initial Catalog=CRUDForm;Integrated Security=True;Encrypt=False");
-            con.Open();
-            SqlCommand cmd = new SqlCommand("Select * From ut Where id=@id", con);
-            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
-            SqlDataAdapter data = new SqlDataAdapter(cmd);
-            DataTable dt = new DataTable();
-            data.Fill(dt);
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Please enter ID to search", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
 
-            dataGridView1.DataSource = dt;
+            using (SqlConnection con = new SqlConnection("Data Source=DESKTOP-IFQAM6G;Initial Catalog=CRUDForm;Integrated Security=True;Encrypt=False"))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * From ut Where id=@id", con);
+                cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+                SqlDataAdapter data = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                data.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
         }
     }
 }
